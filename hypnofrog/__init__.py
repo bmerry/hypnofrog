064608715b97842b47bf4cd436e9d6fed36298a5
Bruce Merry <bmerry@gmail.com>
2020-05-17 20:56:06 +0200

    First version

---
+++ hypnofrog/__init__.py
@@ -0,0 +1,151 @@
+"""Property-based testing for competitive programming.
+
+Still to do:
+- Print timing info and number of runs so far
+- Graceful SIGINT handling
+- Checkers
+"""
+
+import argparse
+import contextlib
+import io
+import subprocess
+import sys
+import resource
+
+from hypothesis import given, settings, Verbosity
+
+
+class FailedCase(Exception):
+    def __init__(self, message, input):
+        super().__init__(message)
+        self.files = {'hypnofrog.in': input}
+        self.logs = {'input': input}
+
+
+class BadAnswer(FailedCase):
+    def __init__(self, message, input, output):
+        super().__init__(message, input)
+        self.files['hypnofrog.out'] = output
+
+
+class AnswerMismatch(BadAnswer):
+    def __init__(self, input, output, reference):
+        super().__init__('Answer does not match the reference', input, output)
+        self.files['hypnofrog.ref'] = reference
+
+
+class CrashError(FailedCase):
+    def __init__(self, input, error):
+        super().__init__(f'Target crashed: {error}', input)
+        self.logs['stderr'] = error.stderr
+
+
+class BadReferenceError(FailedCase):
+    def __init__(self, input, error):
+        super().__init__(f'Reference crashed: {error}', input)
+        self.logs['stderr'] = error.stderr
+
+
+@contextlib.contextmanager
+def capture_stdout():
+    """Context manager that captures standard out, returning a StringIO.
+
+    Use like this::
+
+        with capture_stdout() as stdout:
+            ...
+        print(stdout.getvalue())
+    """
+    out = io.StringIO()
+    real_stdout = sys.stdout
+    sys.stdout = out
+    yield out
+    sys.stdout = real_stdout
+
+
+def make_input(*args):
+    """Convert lines of items into input text.
+
+    Each element of *args corresponds to a line of input. Items that are
+    themselves lists/tuples are space-separated.
+    """
+    return ''.join(
+        (' '.join(str(x) for x in inner) if isinstance(inner, (tuple, list)) else str(inner)) + '\n'
+        for inner in args
+    )
+
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('target', help='Program to test')
+    parser.add_argument('--reference', help='Reference implementation')
+    parser.add_argument('--mem-limit', '-m', type=int, help='Virtual memory limit (MB)')
+    # TODO: add --checker, --base, --files
+    args = parser.parse_args(argv if argv is not None else sys.argv[1:])
+    return args
+
+
+
+def invoke(program, input, mem_limit=None):
+    def preexec():
+        mem_limit_bytes = mem_limit * 1024 * 1024
+        resource.setrlimit(resource.RLIMIT_AS, (mem_limit_bytes, mem_limit_bytes))
+
+    result = subprocess.run(
+        [program],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        input=input,
+        encoding='us-ascii',
+        check=True,
+        close_fds=True,
+        preexec_fn=preexec if mem_limit is not None else None
+    )
+    return result.stdout
+
+
+def trial(input, args):
+    try:
+        try:
+            actual = invoke(args.target, input, args.mem_limit)
+        except subprocess.CalledProcessError as exc:
+            raise CrashError(input, exc) from exc
+        try:
+            expected = invoke(args.reference, input) if args.reference is not None else actual
+        except subprocess.CalledProcessError as exc:
+            raise BadReferenceError(input, exc) from exc
+        if expected != actual:
+            raise AnswerMismatch(input, actual, expected)
+    except FailedCase:
+        raise
+    except Exception as exc:
+        raise FailedCase(f'Unexpected error: {exc}', input) from exc
+
+
+def run(strategy, args=None):
+    @given(strategy)
+    @settings(max_examples=1000000, verbosity=Verbosity.quiet)
+    def test_function(data):
+        trial(data, args)
+
+    if args is None:
+        args = parse_args()
+    try:
+        test_function()
+    except FailedCase as exc:
+        print(exc)
+        for name, content in exc.logs.items():
+            if content:
+                print()
+                print(name)
+                print()
+                print(content.rstrip())
+        print()
+        print('Writing', ', '.join(exc.files.keys()))
+        for name, content in exc.files.items():
+            with open(name, 'w') as f:
+                f.write(content)
+        return 1
+    else:
+        return 0
---
+++ tests/test_hypnofrog.py
@@ -0,0 +1,84 @@
+import argparse
+import os
+
+import pytest
+
+import hypnofrog
+
+
+def solution(basename):
+    return os.path.join(os.path.dirname(__file__), basename)
+
+
+def test_capture_stdout():
+    with hypnofrog.capture_stdout() as stdout:
+        print('Hello world')
+    assert stdout.getvalue() == 'Hello world\n'
+
+
+def test_make_input():
+    input = hypnofrog.make_input(1, [2, 3], (4, 5), 'hello', ['wo', 'rld'])
+    assert input == '1\n2 3\n4 5\nhello\nwo rld\n'
+    assert hypnofrog.make_input() == ''
+
+
+def test_trial_success():
+    args = hypnofrog.parse_args([solution('passthrough'), '--reference', solution('helloworld')])
+    hypnofrog.trial('hello world\n', args)
+
+
+def test_trial_mismatch():
+    args = hypnofrog.parse_args([solution('passthrough'), '--reference', solution('helloworld')])
+    with pytest.raises(hypnofrog.AnswerMismatch) as excinfo:
+        hypnofrog.trial('wrong answer\n', args)
+    assert excinfo.value.files == {
+        'hypnofrog.in': 'wrong answer\n',
+        'hypnofrog.out': '1\nwrong answer\n',
+        'hypnofrog.ref': '1\nhello world\n'
+    }
+    assert excinfo.value.logs == {'input': 'wrong answer\n'}
+
+
+def test_trial_crash():
+    args = hypnofrog.parse_args([solution('crash'), '--reference', solution('helloworld')])
+    with pytest.raises(hypnofrog.CrashError,
+                       match='Target crashed: .* returned non-zero exit status 1') as excinfo:
+        hypnofrog.trial('crash\n', args)
+    assert excinfo.value.files == {'hypnofrog.in': 'crash\n'}
+    assert excinfo.value.logs == {'input': 'crash\n', 'stderr': 'I crash\n'}
+
+
+def test_trial_mem_limit():
+    args = hypnofrog.parse_args([
+        solution('memory_hog'),
+        '--reference', solution('helloworld'),
+        '--mem-limit', '64'
+    ])
+    with pytest.raises(hypnofrog.CrashError):
+        hypnofrog.trial('hello world\n', args)
+    # Make sure it passes without the memory limit
+    args.mem_limit = None
+    hypnofrog.trial('hello world\n', args)
+
+
+def test_trial_bad_reference():
+    args = hypnofrog.parse_args([solution('passthrough'), '--reference', solution('crash')])
+    with pytest.raises(hypnofrog.BadReferenceError,
+                       match='Reference crashed: .* returned non-zero exit status 1') as excinfo:
+        hypnofrog.trial('crash\n', args)
+    assert excinfo.value.files == {'hypnofrog.in': 'crash\n'}
+    assert excinfo.value.logs == {'input': 'crash\n', 'stderr': 'I crash\n'}
+
+
+def test_trial_no_reference():
+    args = hypnofrog.parse_args([solution('passthrough')])
+    hypnofrog.trial('hello world\n', args)
+
+
+def test_trial_no_reference_crash():
+    args = hypnofrog.parse_args([solution('crash')])
+    with pytest.raises(hypnofrog.CrashError,
+                       match='Target crashed: .* returned non-zero exit status 1') as excinfo:
+        hypnofrog.trial('crash\n', args)
+    assert excinfo.value.files == {'hypnofrog.in': 'crash\n'}
+    assert excinfo.value.logs == {'input': 'crash\n', 'stderr': 'I crash\n'}
